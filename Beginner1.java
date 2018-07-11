1b4906025cd78b2d90b06509ba5de9c784a3afb9
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-11 10:46:43 +0530

    Create  Beginner1.java

---
+++ Beginner1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Beginner1
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+if(N>0)
+  System.out.println("positive");
+System.out.println(N<0?"negative":"zero");
+}
+}
