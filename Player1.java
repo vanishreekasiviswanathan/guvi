fd6bd2d6819ae4adcb1fffa7cfdf9385dd9270a2
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-14 20:45:08 +0530

    Create Player1.java

---
+++ Player1.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.Scanner;
+public class playe{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+String str=sc.next();
+if(str.length()>0)
+  for(int i=str.length()-1;i>=0;i--)
+    System.out.print(str.charAt(i));
+else
+	System.out.println(str);
+}
+}
