cdf0a4f9ae8b98e6cea164f1180c308be25b9e5c
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-14 16:59:27 +0530

    Update Pro2.java

--- Pro2.java
+++ Pro2.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-
 public class Pro2 {
-
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		long n=sc.nextLong();
-		int k=sc.nextInt();
+		byte k=sc.nextByte();
 		int len=(""+n).length()-k;
 		ArrayList<String> l=new ArrayList<String>();
-		if(len==0) {
-			System.out.println("0");System.exit(0);}
-		else if(len>0) {
-			for(int p=0,i=0;p<=m.length()-len;p++,i++) {
+		if(k==0)
+		      System.out.println("0");
+		else if(k>0) {
+			for(int i=0;i<=((""+n).length())-len;i++) {
 				l.add(m.substring(i,i+len));
 			}
 			Collections.sort(l);
