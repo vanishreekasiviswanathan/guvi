b6349e19fbb05e0f577afcedae8386845eb96068
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-14 20:46:09 +0530

    Update Player1.java

--- Player1.java
+++ Player1.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 import java.util.Scanner;
-public class playe{
+public class Player1{
 public static void main(String[] args){
 	Scanner sc=new Scanner(System.in);
 String str=sc.next();
