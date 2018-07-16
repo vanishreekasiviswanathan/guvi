885c6ffaf2e3754a537b06f15a47a7047f6376f2
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-16 14:53:38 +0530

    Create Player3.java

---
+++ Player3.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Player3{
+public static void main(String[] arg){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+String str=""+n;
+for(int i=str.length()-1;i>=0;i--)
+  System.out.print(str.charAt(i));
+}
+}
