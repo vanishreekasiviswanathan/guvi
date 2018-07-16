d9c6c589d724c4d47421092140e92d42e742e0a1
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-16 20:00:37 +0530

    Update Beginner5.java

--- Beginner5.java
+++ Beginner5.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Beginner5{
 public static void main(String[] arg){
 Scanner s=new Scanner(System.in);
-int max=0;
-for(int i=0;i<3;i++){
+int max=sc.nextInt();
+for(int i=0;i<2;i++){
   int n=s.nextInt();
   if(n>max)
     max=n;
