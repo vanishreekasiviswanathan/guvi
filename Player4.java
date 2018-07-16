da5b63b15fb1dd3435fb0be83740513db0bfacbc
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-16 15:22:19 +0530

    Create Player4.java

---
+++ Player4.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Player4{
+public static void main(String[] arg)
+{
+  Scanner sc=new Scanner(System.in);
+  String str=sc.next();
+  System.out.println(str+".");
+}
+}
