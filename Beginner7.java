1b4557347991f35b32de6e327c74ea0a2bdbd642
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-16 20:08:04 +0530

    Create Beginner7.java

---
+++ Beginner7.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Beginner7{
+public static void main(String[] arg){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<n;i++)
+      System.out.println("Hello");
+}
+}
