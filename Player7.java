0467991794e80093af0ade9ce421e4e1b7ea53f8
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-22 11:12:59 +0530

    Create Player7.java

---
+++ Player7.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Player7
+{
+public static void main(String[] args)
+{
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  String res="";
+  for(int i=0;i<str.length()-1;i+=2)
+  {
+  StringBuilder sb=new StringBuilder(str.substring(i,i+2));
+  res+=sb.reverse();
+  }
+  System.out.println(res);
+}
+}
