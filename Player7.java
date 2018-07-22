f00cec07d9781ba04211d4e9f47160b622a5cbc4
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-22 11:26:38 +0530

    Update Player7.java

--- Player7.java
+++ Player7.java
@@ -12,6 +12,8 @@ public static void main(String[] args)
   StringBuilder sb=new StringBuilder(str.substring(i,i+2));
   res+=sb.reverse();
   }
+  if(str.length()%2==1)
+    res+=str.charAt(str.length()-1);
   System.out.println(res);
 }
 }
