12c2fcff8d7a1dd6b00d68e130da8180f985e414
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-23 16:38:39 +0530

    Create Beginner10.java

---
+++ Beginner10.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Beginner10
+{
+public static void main(String[] args)
+{
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  System.out.println(String.valueOf(n).length());
+  }
+}
