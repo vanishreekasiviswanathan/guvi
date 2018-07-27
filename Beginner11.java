d96bb0c8042f9d3cc78dc5a40fb8fa4fb351d9df
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-27 16:57:15 +0530

    Create Beginner11.java

---
+++ Beginner11.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Beginner11{
+public static void main(String[] arg)
+{
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int p=sc.nextInt();
+  System.out.println(Math.pow(n,p));
+}
+}
