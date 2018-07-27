be1e7dd3d502f6aabdf1a33b646cc296a576334b
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-07-27 17:16:38 +0530

    Create Beginner20.java

---
+++ Beginner20.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Beginner20{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=5;i++)
+  System.out.print((n*i)+" ");
+}
+}
