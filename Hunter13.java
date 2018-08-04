0d2aad832a3c58ab79ea27aca09c90948c73209b
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-08-04 11:13:02 +0530

    Create Hunter13.java

---
+++ Hunter13.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Hunter13{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  String str=sc.nextLine();
+  char[] ch=str.toCharArray();
+  LinkedList<Character> ll=new LinkedList<Character>();
+  for(Character c:ch)
+    ll.add(c);
+ for(int j=str.length()-1,i=0;j>=0;j--)
+   ch[i++]=str.charAt(j);
+   int k=0;
+   int count=0;
+  for(Character m:ll)
+  {
+    if(m==ch[k++])
+      count++;
+  }
+  if(count==ch.length)
+    System.out.println("yes");
+    else
+      System.out.println("no");
+ }
+}
