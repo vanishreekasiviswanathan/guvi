5634508d9eba093d10d423adfbdaeb43b7efa1d6
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-08-10 19:11:13 +0530

    Create Hunter29.java

---
+++ Hunter29.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Hunter29
+{
+public static void main(String[] args)
+{
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int[] a=new int[n];
+  for(int i=n-1;i>=0;i--)
+  {
+    for(int j=0;j<=i;j++)
+    {
+      for(int k=j;k)
+    }
+  }
+}
+}
