bd327ae24123dbca8f83db04dd5cd773dd4d7265
vanishreekasiviswanathan <41100899+vanishreekasiviswanathan@users.noreply.github.com>
2018-08-14 23:18:57 -0400

    Create Player21.java

---
+++ Player21.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.Scanner;
+public class Player21
+{
+public static void main(String[] args)
+{
+ Scanner sc=new Scanner(System.in);
+ int[] a=new int[2];
+ int[] b=new int[2];
+ int[] c=new int[2];
+ a[0]=sc.nextInt();
+ a[1]=sc.nextInt();
+ b[0]=sc.nextInt();
+ b[1]=sc.nextInt();
+ c[0]=sc.nextInt();
+ c[1]=sc.nextInt();
+ boolean flag=false;
+ for(int i=0;i<2;i++){
+      if(a[i]==b[i] &&b[i]==c[i])
+       {
+        flag=true;break;
+        }
+   }
+   System.out.println((flag)?"yes":"no");
+}
+}
